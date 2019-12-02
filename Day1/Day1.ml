5e2060b9e94513a1bb84bcb0fbc726a1bbd9be1d
Tristan Deloche <tristan.deloche@treatwell.com>
2019-12-02 16:41:06 +0000

    Finish day1

--- Day1/Day1.ml
+++ Day1/Day1.ml
@@ -3,12 +3,25 @@ open Commons
 let mass_to_fuel(mass: int): int =
   int_of_float (floor ( (float_of_int mass) /. 3.0 ) ) - 2
 
-let () = 
+let rec mass_to_fuel_2(accounted: int)(newly_added_fuel: int): int =
+  let newly_added_fuel_fuel = mass_to_fuel newly_added_fuel in
+  match (newly_added_fuel_fuel <= 0) with
+   | true -> accounted
+   | false -> mass_to_fuel_2 (accounted + newly_added_fuel_fuel) (newly_added_fuel_fuel)
+
+let masses: (int list) = 
   let list_of_modules = IOUtils.read_all_lines "./Day1/input" in
-  let list_of_module_masses = List.map int_of_string list_of_modules in
-  let list_of_fuel_reqs = List.map mass_to_fuel list_of_module_masses in
-  let rec sum(l: (int list)) =
-    match l with
-      | [] -> 0
-      | h::t -> h + sum t
-  in print_int (sum list_of_fuel_reqs)
+  List.map int_of_string list_of_modules
+
+let total_fuel_using(part: int) (mass_to_fuel_op: (int -> int)): unit =
+  let list_of_fuel_reqs = List.map mass_to_fuel_op masses in
+  let total_fuel = List.fold_left (fun a b -> a + b) 0 list_of_fuel_reqs in
+  Printf.printf "Part %d: %d\n" part total_fuel
+
+let part_1(): unit = total_fuel_using 1 mass_to_fuel
+
+let part_2(): unit = total_fuel_using 2 (mass_to_fuel_2 0)
+
+let () =
+  part_1();
+  part_2();
