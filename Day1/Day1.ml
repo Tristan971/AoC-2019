4c07553c3e6cec467ba4432f82d154a557fb852e
Tristan Deloche <tristan.deloche@treatwell.com>
2019-12-02 16:15:05 +0000

    Add basis of day1

--- Day1/Day1.ml
+++ Day1/Day1.ml
@@ -1,5 +1,14 @@
 open Commons
 
+let mass_to_fuel(mass: int): int =
+  int_of_float (floor ( (float_of_int mass) /. 3.0 ) ) - 2
+
 let () = 
   let list_of_modules = IOUtils.read_all_lines "./Day1/input" in
-  print_string "Read "; print_int (List.length list_of_modules); print_string " modules"
+  let list_of_module_masses = List.map int_of_string list_of_modules in
+  let list_of_fuel_reqs = List.map mass_to_fuel list_of_module_masses in
+  let rec sum(l: (int list)) =
+    match l with
+      | [] -> 0
+      | h::t -> h + sum t
+  in print_int (sum list_of_fuel_reqs)
