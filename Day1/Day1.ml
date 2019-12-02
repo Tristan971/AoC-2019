d8f06953fcdf61ff52faf32080b427adedaded1b
Tristan Deloche <tristan.deloche@treatwell.com>
2019-12-02 16:05:36 +0000

    Actually add input

--- Day1/Day1.ml
+++ Day1/Day1.ml
@@ -1,5 +1,5 @@
 open Commons
 
 let () = 
-  let list_of_modules = IOUtils.read_all_lines "test" in
+  let list_of_modules = IOUtils.read_all_lines "./Day1/input" in
   print_string "Read "; print_int (List.length list_of_modules); print_string " modules"
