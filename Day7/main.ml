08986eac7a88aca0dace8d603e93234fe6bd19b6
Tristan Deloche <tristan.deloche@treatwell.com>
2019-12-12 13:43:09 +0000

    Cleanup outputs

--- Day7/main.ml
+++ Day7/main.ml
@@ -1,16 +1,36 @@
 open Commons
+open Commons.Basics
 
 let input: string list = IOUtils.read_all_lines "Day7/input"
 
-let exec_program(program: string) (inputs: string): int =
-  let result = IntcodeComputer.execute (IntcodeComputer.intcode_program_of program inputs) in
+let exec_program(program: string) (inputs: int list): int =
+  let memory_init = split_as_ints program in
+  let intcode_program: IntcodeComputer.intcode_program = {
+    memory = memory_init;
+    inputs = inputs
+  } in
+
+  let result = IntcodeComputer.execute intcode_program in
   let output = List.hd result.outputs in
-  Printf.printf "Finished with output: %d and array:\n" output;
-  Basics.print_int_array result.memory 0;
+  (* Printf.printf "Finished with output: %d and array: " output;
+  Basics.print_int_array result.memory 0; *)
   output
 
+let do_amplification (program: string) (phases: int list): int =
+  let rec amplify (phases: int list) (previous: int) =
+    match phases with
+    | [] -> previous
+    | phase::remaining_phases -> let result = exec_program program [phase; previous] in amplify remaining_phases result
+  in amplify phases 0
+
+let run_sample (program: string) (phases: int list): unit =
+  let result = do_amplification program phases in
+  let name = String.concat "" (List.map string_of_int phases) in
+  Printf.printf "\n- AMP for %s => %d\n" name result
+
 let samples() =
-  exec_program "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" "4,3,2,1,0"
+  run_sample "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" [4; 3; 2; 1; 0];
+  print_endline "Samples over"
 
 let () =
   let _ = samples() in
--- commons/IntcodeComputer.ml
+++ commons/IntcodeComputer.ml
@@ -52,7 +52,7 @@ let instruction_input : instruction = { opcode = 3; param_count = 1 }
 
 let execution_input (input : int) (e : execution) : execution_result =
   let _, output = List.hd e.params_and_mode in
-  print_string "INPUT REQUEST: ";
+  (* print_string "INPUT REQUEST: "; *)
   e.array.(output) <- input;
   exec_result_of_next_pos (e.position + 2)
 
@@ -61,7 +61,7 @@ let instruction_print : instruction = { opcode = 4; param_count = 1 }
 let execution_print (e : execution) : execution_result =
   let mi, pi = List.hd e.params_and_mode in
   let input = if mi == 0 then e.array.(pi) else pi in
-  Printf.printf "\n[ OUTPUT ] => %d\n" input;
+  Printf.printf "Output => %d\n" input;
   { next_position = e.position + 2; output = Some input }
 
 let instruction_jmp_if_true : instruction = { opcode = 5; param_count = 2 }
@@ -193,10 +193,10 @@ let read_execution (arr : int array) (position : int) : execution =
 
 let apply_execution (e : execution) (inputs : int list) :
     execution_result * int list =
-  Printf.printf "Executing opcode %d @ %d with %d and params: [ "
+  (* Printf.printf "Executing opcode %d @ %d with %d and params: [ "
     e.instruction.opcode e.position e.instruction.opcode;
   List.iter (fun (i1, i2) -> Printf.printf " %d|%d " i1 i2) e.params_and_mode;
-  Printf.printf " ]\n";
+  Printf.printf " ]\n"; *)
   let new_inputs : int list =
     if e.instruction.opcode == instruction_input.opcode then (
       Printf.printf "Consuming input %d\n" (List.hd inputs);
@@ -232,19 +232,19 @@ let intcode_program_of (memory : string) (inputs : string) : intcode_program =
 type intcode_program_result = { memory : int array; outputs : int list }
 
 let execute (program : intcode_program) : intcode_program_result =
-  print_string "\n\n-- Starting computer --\n";
-  print_string "| Input array: ";
+  print_string "\n\n-- Start --\n";
+  (* print_string "| Input array: ";
   Basics.print_int_array program.memory 0;
-  Printf.printf "-----------------------\n";
+  Printf.printf "-----------------------\n"; *)
 
   let rec execute_from (position : int) (inputs : int list) (outputs : int list)
       : int list * int array =
     let execution = read_execution program.memory position in
     match execution.instruction with
     | instruction when instruction == instruction_halt ->
-        Printf.printf "Got HALT @ %d\n" position;
-        print_string "Result: ";
-        Basics.print_int_array program.memory 0;
+        Printf.printf "Got HALT @ %d\n-- Done --\n\n" position;
+        (* print_string "Result: "; *)
+        (* Basics.print_int_array program.memory 0; *)
         (outputs, program.memory)
     | __ ->
         let exec_result, remaining_inputs = apply_execution execution inputs in
@@ -256,7 +256,7 @@ let execute (program : intcode_program) : intcode_program_result =
         execute_from exec_result.next_position remaining_inputs new_outputs
   in
   let outputs, memory = execute_from 0 program.inputs [] in
-  Printf.printf "-- Finished. --\n\n";
+  (* Printf.printf "-- Finished. --\n\n"; *)
   { memory; outputs }
 
 let () = print_endline "Loaded IntCode virtual machine"
