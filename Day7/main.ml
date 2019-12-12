1d4f0bd379f51f62ab391016062376bd14cd20ed
Tristan Deloche <tristan.deloche@treatwell.com>
2019-12-12 13:44:45 +0000

    Add samples

--- Day7/main.ml
+++ Day7/main.ml
@@ -1,14 +1,13 @@
 open Commons
 open Commons.Basics
 
-let input: string list = IOUtils.read_all_lines "Day7/input"
+let input : string list = IOUtils.read_all_lines "Day7/input"
 
-let exec_program(program: string) (inputs: int list): int =
+let exec_program (program : string) (inputs : int list) : int =
   let memory_init = split_as_ints program in
-  let intcode_program: IntcodeComputer.intcode_program = {
-    memory = memory_init;
-    inputs = inputs
-  } in
+  let intcode_program : IntcodeComputer.intcode_program =
+    { memory = memory_init; inputs }
+  in
 
   let result = IntcodeComputer.execute intcode_program in
   let output = List.hd result.outputs in
@@ -16,22 +15,31 @@ let exec_program(program: string) (inputs: int list): int =
   Basics.print_int_array result.memory 0; *)
   output
 
-let do_amplification (program: string) (phases: int list): int =
-  let rec amplify (phases: int list) (previous: int) =
+let do_amplification (program : string) (phases : int list) : int =
+  let rec amplify (phases : int list) (previous : int) =
     match phases with
     | [] -> previous
-    | phase::remaining_phases -> let result = exec_program program [phase; previous] in amplify remaining_phases result
-  in amplify phases 0
+    | phase :: remaining_phases ->
+        let result = exec_program program [ phase; previous ] in
+        amplify remaining_phases result
+  in
+  amplify phases 0
 
-let run_sample (program: string) (phases: int list): unit =
+let run_sample (program : string) (phases : int list) : unit =
   let result = do_amplification program phases in
   let name = String.concat "" (List.map string_of_int phases) in
   Printf.printf "\n- AMP for %s => %d\n" name result
 
-let samples() =
-  run_sample "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" [4; 3; 2; 1; 0];
+let samples () =
+  run_sample "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" [ 4; 3; 2; 1; 0 ];
+  run_sample
+    "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+    [ 0; 1; 2; 3; 4 ];
+  run_sample
+    "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+    [ 1; 0; 4; 3; 2 ];
   print_endline "Samples over"
 
 let () =
-  let _ = samples() in
+  let _ = samples () in
   print_endline "Done"
